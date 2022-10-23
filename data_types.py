3cc145766579237ae7adf242e830174eb569609d
4ortq <papa4ertei@gmail.com>
2022-10-23 17:26:59 +0300

    second homework 23.10.2022

---
+++ data_types.py
@@ -0,0 +1,12 @@
+temp_in= 1000
+temp_out= 200
+my_result=10
+part_number=405
+power_in=220
+first_name='Artur'
+second_name= 'Piroshkov'
+name_win= 'Kateryna'
+user_nick='ababa'
+hit_point='head'
+print (temp_in,temp_out,my_result,part_number,power_in,
+       first_name,second_name,name_win,user_nick,hit_point)
