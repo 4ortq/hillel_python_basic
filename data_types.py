a4da60ced90843bef8b924e32277800c681ce3fe
4ortq <papa4ertei@gmail.com>
2022-10-24 18:30:46 +0300

    secondhomework/v2

--- data_types.py
+++ data_types.py
@@ -1,12 +1,31 @@
 temp_in= 1000
+print(type(temp_in))
+print(temp_in)
 temp_out= 200
+print(type(temp_out))
+print(temp_out)
 my_result=10
+print(type(my_result))
+print(my_result)
 part_number=405
+print(type(part_number))
+print(part_number)
 power_in=220
+print(type(power_in))
+print(power_in)
 first_name='Artur'
+print(type(first_name))
+print(first_name)
 second_name= 'Piroshkov'
+print(type(second_name))
+print(second_name)
 name_win= 'Kateryna'
+print(type(name_win))
+print(name_win)
 user_nick='ababa'
+print(type(user_nick))
+print(user_nick)
 hit_point='head'
-print (temp_in,temp_out,my_result,part_number,power_in,
-       first_name,second_name,name_win,user_nick,hit_point)
+print(type(hit_point))
+print(hit_point)
+
