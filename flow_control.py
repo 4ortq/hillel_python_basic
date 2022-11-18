5b2037fcd4b281517eba44a49ca2727e6e5add7b
4ortq <papa4ertei@gmail.com>
2022-11-18 20:30:29 +0200

    homework flow_control v2

--- flow_control.py
+++ flow_control.py
@@ -1,37 +1,44 @@
 numb_1 = input ("give me number")
-try:
-     y=int(numb_1)
-except:
-     y=float(numb_1)
+y=0
+if str.isdigit(numb_1) == True:
+        y = int(numb_1)
+elif str.isdigit(numb_1) == False:
+        y = float(numb_1)
 print("type operand 1 >>> ", type(y))
 numb_2 = input ("give me number")
-try:
-     z=int(numb_2)
-except:
-     z=float(numb_2)
+z = 0
+if str.isdigit(numb_2) == True:
+        z = int(numb_2)
+elif str.isdigit(numb_2) == False:
+        z = float(numb_2)
 print("type operand 2 >>> ", type(z))
 work_process = input("what i have to do * / + - **")
 if work_process in "*":
-    result = y * z
+        result = y * z
 elif work_process in "/":
-    result = y / z
+        result = y / z
 elif work_process in "+":
-    result = y + z
+        result = y + z
 elif work_process in "-":
-    result = y - z
+        result = y - z
 elif work_process in "**":
-    result = y ** z
+        result = y ** z
 else:  raise Exception("ALARM!!! incorect enter")
-# if y == type(int) \                  # its not work
-#         and z == type(int):          # its not work
-#         finelly = int(result)        # its not work
+if y == int and z == int:
+    result = int
+elif y == float  and z == float:
+    result = float
+division_int_y = str(round(y))
+division_int_z = str(round(z))
+division_int_result = str(round(result))
+print(f" order result >>> {len(division_int_result)},order z >>> {len(division_int_z)},order y >>> {len(division_int_y)}")
 print("result >>>", result)
 print("type result>>>", type(result))
-division_int_y = str(int(y))
-print("orders numb_1 >>>", len(division_int_y))
-division_int_z = str(int(z))
-print("orders numb_2 >>>", len(division_int_z))
-division_int_result = str(int(result))
-print("orders result >>>", len(division_int_result))
+# division_int_y = str(int(y))
+# print("orders numb_1 >>>", len(division_int_y))
+# division_int_z = str(int(z))
+# print("orders numb_2 >>>", len(division_int_z))
+# division_int_result = str(int(result))
+# print("orders result >>>", len(division_int_result))
 print("comparing type operands operand1-operand2, operand2-result, operand1-result>>>", type(y) == type(z), type(z) == type(result), type(y) == type(result))
 #######################################################
