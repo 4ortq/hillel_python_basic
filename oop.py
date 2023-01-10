4ebc9276446939c245ea84993a783cf7e23be8e8
4ortq <papa4ertei@gmail.com>
2023-01-10 21:17:07 +0200

    ver-1.0

---
+++ oop.py
@@ -0,0 +1,41 @@
+class гшnit:
+    def __init__(self, hp, mp, armor, magic_resist):
+        self.hp = hp
+        self.mp = mp
+        self.armor = armor
+        self.magic_resist = magic_resist
+    def atack
+class Archer(unit):
+    def __init__(self, hp, armor, magic_resist, attack):
+        self.hp = hp
+        self.armor = armor
+        self.magic_resist = magic_resist
+        self.atack = attack
+
+    def atack:
+
+
+
+
+class Mage:
+
+class Knight:
+
+class Healer:
+
+check_red = 0
+check_blue = 0
+check = input(f'red or blue')
+# check_red = 1 if input(f"red or blue") == red else check_blue = 1
+if check == "red":
+    check_red = 1
+else: check_blue = 1
+
+while True:
+    if check_red > check_blue:
+        print(f"yup")
+        check_blue += 1
+    else:
+        print(f'nope')
+        check_red += 1
+
