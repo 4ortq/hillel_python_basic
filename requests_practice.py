401fae764e60a1a39200d578460dc3fa127a406a
4ortq <papa4ertei@gmail.com>
2023-01-19 18:09:46 +0200

    requests v1.1

--- requests_practice.py
+++ requests_practice.py
@@ -1,13 +1,19 @@
 import requests
 
+
 url_data = 'https://jsonplaceholder.typicode.com/posts'
 url_picture = 'https://www.meme-arsenal.com/memes/52365d75819cd9aebdb4c09db8e0d24f.jpg'
-res = requests.get(url = url_data)
-with open('content.txt', 'w') as f:
-    f.write(res.text)
-    print(res.json())
+
+def request_data(data):
+    res = requests.get(url = data)
+    with open('content.txt', 'w') as f:
+        f.write(res.text)
+        print(res.json())
 ########################################################################################################
-r = requests.get(url=url_picture)
-with open('picture.jpg','wb') as f:
-    f.write(r.content)
+def requst_picture(picture):
+    r = requests.get(url=picture)
+    with open('picture.jpg','wb') as f:
+        f.write(r.content)
+request_data(url_data)
+requst_picture(url_picture)
 
