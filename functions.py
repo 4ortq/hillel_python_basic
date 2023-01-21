5155fe56dbec73f894bab1110f6d035a9407ac88
4ortq <papa4ertei@gmail.com>
2023-01-21 17:14:36 +0200

    fix func 1,2, 3.1, 3.2

--- functions.py
+++ functions.py
@@ -3,123 +3,80 @@
 # 3.Створи три функції, які обчислюють суму чисел у списку
 # - з for-циклом
 # - з while-циклом
-# - з рекурсією
-# Створи функцію яка обчислює числа Фібоначчі. Функція повертає число з послідовності за порядковим номером
-# Надрукуй 4 рядки: помідор -> м'ясо -> сир -> хліб, умови:
-# Створи 4 функції, кожна друкує свій шар
-# Викликай лише одну функцію
-# Використай синтаксис декораторів
-# Очікуваний результат друку:
-# помідор
-# м'ясо
-# сир
-# хліб
 # ##################################
-# Start = int(input("give me start"))
-# Stop = int(input("give me stop"))
-# print(f"step = 1 default ")
-# if Start > Stop:
-#         a = Stop
-#         b = Start
-# else:
-#         a = Start
-#         b = Stop
-# def sum_number(start, stop):
-#     seq = range(start, stop+1)
-#     summ = sum(seq)
-#     return summ
-# print(sum_number(a,b))
+start = int(input("give me start"))
+stop = int(input("give me stop"))
+def sum_number(start, stop):
+    print(f"step = 1 default ")
+    if start > stop:
+        a = stop
+        b = start
+    else:
+        a = start
+        b = stop
+    seq = sum(range(a, b+1))
+    return seq
+print(sum_number(start,stop))
 #########################################
-# Second_data = input("write a second") # рахує як лічільник
-# def day_in_year(seq):
-#     seq = int(seq)
-#     second_value = seq % 60
-#     year_value = 0
-#     day_value = 0
-#     hour_value = 0
-#     minutes_value = 0
-#     while seq >= 60:
-#         if seq >= 3153000:
-#             year_value += 1
-#             seq = seq - 3153000
-#         elif seq >= 86400:
-#             day_value += 1
-#             seq = seq - 86400
-#         elif seq >= 3600:
-#             hour_value += 1
-#             seq = seq - 3600
-#         elif seq >=  60:
-#             minutes_value += 1
-#             seq = seq - 60
-#     return  { "year" : year_value,
-#               "day" :day_value,
-#               "hour":hour_value,
-#               "minutes":minutes_value,
-#               "second":second_value }
-# print(day_in_year(Second_data))
-#######################################
-# Second_data = input("write a second") # рахуй цілочислельним діленням та залишком від ділення
-# def day_in_year(seq):
-#     if type(seq) == str:
-#         seq = int(seq)
-#     else: pass
-#     year_value = 0
-#     day_value = 0
-#     hour_value = 0
-#     minutes_value = 0
-#     second_value = seq % 60
-#     seq = seq - second_value
-#     while seq >= 60:
-#         if seq >= 3153000:
-#             year_value = seq // 3153000
-#             seq = seq - (year_value * 3153000)
-#         elif seq >= 86400:
-#             day_value = seq // 86400
-#             seq = seq - (day_value * 86400)
-#         elif seq >= 3600:
-#             hour_value = seq // 3600
-#             seq = seq - (hour_value * 3600)
-#         elif seq >=  60:
-#             minutes_value = seq // 60
-#             seq = seq - (minutes_value * 60)
-#     return  { "year" : year_value,
-#               "day" :day_value,
-#               "hour":hour_value,
-#               "minutes":minutes_value,
-#               "second":second_value }
-# print(day_in_year(Second_data))
+second_data = int(input("write a second")) # рахуй цілочислельним діленням та залишком від ділення
+
+
+def day_in_year(seq):
+    second_value = seq % 60
+    seq = seq - second_value
+    while seq >= 60:
+        if seq >= 86400:
+            day_value = seq // 86400
+            seq = seq - (day_value * 86400)
+        if seq >= 3600:
+            hour_value = seq // 3600
+            seq = seq - (hour_value * 3600)
+        if seq >= 60:
+            minutes_value = seq // 60
+            seq = seq - (minutes_value * 60)
+        else: break
+    return print(f'{day_value}:{hour_value}:{minutes_value}:{second_value}')
+
+
+print(day_in_year(second_data))
 ###############################################################
-# data_list = []
-# for el in range(1,26):
-#     data_list.append(el)
-# print(data_list)
-# # def total_list_for(seq):
-# #     total = 0
-# #     for el in seq:
-# #         total = total + el
-# #     return total
-# # print(total_list_for(data_list))
-# # def total_list_while(seq):
-# #     total = 0
-# #     el = 0
-# #     while el < len(seq):
-# #         total = total + seq[el]
-# #         el = el + 1
-# #     return total
-# # print(total_list_while(data_list))
-# total = 0
-# def recursion(x):
-#     global total
-#     if x >= len(data_list):
-#        return
-#     total += data_list[x]
-#     recursion(x+1)
-# recursion(0)
-# print(total)
-# print(len(data_list))
+data_list = []
+for el in range(1,26):
+    data_list.append(el)
+print(data_list)
+
+
+def total_list_for(seq):
+    total = 0
+    for el in seq:
+        total = total + el
+    return total
+print(total_list_for(data_list))
+
+
+def total_list_while(list):
+    total = 0
+    while len(list) > 0:
+        total += list.pop()
+    return total
+
+
+print(total_list_while(data_list))
+
+
+# def recursion(number, index=0):
+#     if index >= len(number):
+#         return 0
+#     else:
+#         return number[index] + recursion(number, index+1)
+#
+#
+# print(recursion(data_list))
 # ###################################################################
 # numb = int(input("write a number for fibonachi"))
 # dict_fib = {0: 0, 1: 1}
+#
+#
 # def fib(n):
 #     if n in dict_fib:
 #         return dict_fib[n]
@@ -130,19 +87,23 @@
 def main_decor_sandwich(func):
     print(f'Decorating function')
 
-    def decorator_tomatos():
-        print(f"Помідор")
 
-    def decorator_meat():
-        print(f"м'ясо")
+def decorator_tomatos():
+    print(f"Помідор")
 
-    def decorator_chees():
-        print(f"Сир")
 
-    def decorator_bread():
-        print(f"Хліб")
+def decorator_meat():
+    print(f"м'ясо")
 
-    return decorator_tomatos(),decorator_meat(),decorator_chees(),decorator_bread()
+
+def decorator_chees():
+    print(f"Сир")
+
+
+def decorator_bread():
+    print(f"Хліб")
+
+
 
 @main_decor_sandwich
 def main_func():
